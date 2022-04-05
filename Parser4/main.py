8c0a7ef12ed88b599665685e8038d1f967b8b719
DenKwuh2 <79425501+DenKwuh2@users.noreply.github.com>
2022-04-05 17:24:38 +0300

    Parser 4

--- Parser4/main.py
+++ Parser4/main.py
@@ -5,7 +5,7 @@ import time
 
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36 OPR/85.0.4341.47",
+    "User-Agent": "...",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchang"
 }
 
